34da1e373fb9bbcd4a7313ba5e46ac8a52583d7c
farelsatrio <farelsatriop@gmail.com>
2026-06-18 15:05:56 +0700

    init

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,6 +4,5 @@ import { routing } from "./i18n/routing";
 export const proxy = createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ["/", "/(id|en)/:path*"],
 };
